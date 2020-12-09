1993f7acc39fb0b24fe5cbc52f29d51d6a2457f3
Ikenna  Okoli <iceokoli@yahoo.com>
2020-12-09 19:02:55 +0000

    day 7

--- Day 7/day7.go
+++ Day 7/day7.go
@@ -8,8 +8,25 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"strconv"
 	"strings"
 )
+
+func appendNoDupes(a []string, b []string) []string {
+
+	check := make(map[string]int)
+	d := append(a, b...)
+	res := make([]string, 0)
+	for _, val := range d {
+		check[val] = 1
+	}
+
+	for letter := range check {
+		res = append(res, letter)
+	}
+
+	return res
+}
 
 func readInput() []string {
 	// Get current directory
@@ -19,7 +36,7 @@ func readInput() []string {
 		log.Fatal(err)
 	}
 	// read in the file
-	raw, err := ioutil.ReadFile(dir + "/test.txt")
+	raw, err := ioutil.ReadFile(dir + "/input.txt")
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -33,33 +50,60 @@ func formatData(data []string) map[string][]string {
 
 	res := make(map[string][]string)
 	for _, i := range data {
-		var idx = regexp.MustCompile("^\\w+\\s\\w+\\s\\w+")
+		var idx = regexp.MustCompile("^\\w+\\s\\w+")
 		var elm = regexp.MustCompile("[0-9]+\\s\\w+\\s\\w+\\s\\w+[^\\.|,]")
 		res[idx.FindString(i)] = elm.FindAllString(i, -1)
 	}
 	return res
 }
 
-func findGoldBag(data map[string][]string) int {
+func findBag(color string, data map[string][]string) []string {
 	// find surface level gold bags
-	var cnt int
-	for _, v := range data {
+	var baseColors []string
+	for k, v := range data {
 		for _, i := range v {
-			if strings.Contains(i, "shiny gold") {
-				cnt = cnt + 1
+			if strings.Contains(i, color) {
+				baseColors = append(baseColors, k)
 			}
 		}
 	}
 
-	// find nested bags
+	var result []string
+	if len(baseColors) == 0 {
+		return baseColors
+	}
 
-	return cnt
+	for _, i := range baseColors {
+		temp := findBag(i, data)
+		result = appendNoDupes(result, temp)
+	}
+
+	result = appendNoDupes(result, baseColors)
+	return result
+}
+
+func findNoBag(color string, data map[string][]string) float64 {
+
+	var noBag float64
+	if len(data[color]) == 0 {
+		return 0
+	}
+
+	for _, i := range data[color] {
+		n, _ := strconv.ParseFloat(i[:1], 64)
+		clr := strings.Join(strings.Split(i, " ")[1:3], " ")
+		noBag = noBag + (n * findNoBag(clr, data)) + n
+	}
+
+	return noBag
 }
 
 func main() {
 
 	raw := readInput()
 	clean := formatData(raw)
-	fmt.Println(findGoldBag(clean))
+	partA := findBag("shiny gold", clean)
+	fmt.Println("Part A: ", len(partA))
+	fmt.Println("Part B: ", findNoBag("shiny gold", clean))
 
 }
